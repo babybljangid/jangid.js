b3fd01b3d7ea3fd52aefb066fcf76e8f57457aa5
babybljangid <145833134+babybljangid@users.noreply.github.com>
2023-09-23 14:32:30 +0000

    datatypes

---
+++ basicsJS/DataTypes.js
@@ -0,0 +1,12 @@
+
+
+let name = "jangid" /*string*/
+let age = 22 /*number*/
+let isLoggedIn = true /*boolean  true/false */
+let state = null //empty value output = object
+let a; //undefined
+//symbol = unique
+
+console.log(typeof "jangid");
+console.log(typeof null);
+
